e1b6a61a845d82f1a81f44b498225bfbdcf328d3
jpssh <jpsah44@gmail.com>
2024-03-02 21:36:23 +0530

    hello

---
+++ about.php
@@ -0,0 +1,189 @@
+<?php include "header.php"; ?>
+	<!-- Start header -->
+	<img src="images/banner_about_us.jpg" width="100%">
+	<!-- End header -->
+	
+	<!-- Start About -->
+	<div class="about-section-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 text-center">
+					<div class="inner-column">
+						<h1>Welcome To <span>We'Restro</span></h1>
+						<h4>Little Story</h4>
+						<p> Discover a culinary journey like no other at We'Restro, nestled in the heart of Bangalore. Since our establishment in 2023, We'Restro has been a haven for food enthusiasts seeking a unique blend of flavors and a welcoming atmosphere. </p>
+						<p>Behind every exceptional dining experience is an exceptional team. Get to know the creative minds in our kitchen, the attentive faces serving your table, and the passionate individuals who make We'Restro a culinary destination. </p>
+						<!--<a class="btn btn-lg btn-circle btn-outline-new-white" href="#"> Comment</a> -->
+					</div>
+				</div>
+				<div class="col-lg-6 col-md-6">
+					<img src="images/images.jpg" style="height: 450px; width: 100%" class="img-fluid">
+				</div>
+				<div class="col-md-12">
+					<div class="inner-pt">
+						<p> Making way for a hearty meal is We'Restro in Bangalore. This place is synonymous with delicious food that can satiate all food cravings. It is home to some of the most appreciated cuisines. So as to be able to cater to a large number of diners, it occupies a favourable location at South Bangalore. Opposite Skoda Workshop,Ring Road,South Bangalore-380058 is where one can visit the venue. Courtesy to this strategic location, foodies in and around the neighborhood can walk in to this eating house conveniently without facing any hassles related to commuting to this part of the city. It is one of the most sought after Restaurants in South Bopal. This is a one of the renowned Restaurants in Bangalore.
+
+ </p>
+						<p>We invite you to explore our menu, make a reservation, or simply drop by to experience the magic of We'Restro. As a token of our appreciation, you can get free drinks on ocassion of diwali.
+
+Thank you for being part of the We'Restro story. We look forward to serving you soon!
+ </p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End About -->
+	
+	<!-- Start Menu -->
+	<div class="menu-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Special Menu</h2>
+						<p>We provide everthing tasty but our special menu is north food.</p>
+					</div>
+				</div>
+			</div>
+				
+			<div class="row inner-menu-box">
+				<div class="col-3">
+					<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+						<a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">All</a>
+						<a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">KathiyaWadi</a>
+						<a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">RajsThani</</a>
+						<a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Rise</a>
+
+						<a class="nav-link" id="v-pills-tava-tab" data-toggle="pill" href="#v-pills-tava" role="tab" aria-controls="v-pills-tava" aria-selected="false">tava</a>
+					</div>
+				</div>
+				
+				<div class="col-9">
+					<div class="tab-content" id="v-pills-tabContent">
+						<div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+							<div class="row">
+								<?php include "connect.php";
+								$s = mysqli_query($con,"select * from menu");
+								while($r = mysqli_fetch_array($s))
+								{	
+								?>
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="admin/<?php echo $r['image']; ?>" class="img-fluid" alt="Image" style='width: 263px; height:170px;'>
+										<div class="why-text">
+											<h4><?php echo $r['title']; ?></h4>
+											<p><?php echo $r['description']; ?></p>
+											<p>Rs <?php echo $r['price']; ?> /-</p>
+											<h5 align="center">
+									<?php if(isset($_SESSION['uid']))
+									{
+									?>
+											<a href="addcart.php?pid=<?php echo $r['id']; ?>&uid=<?php echo $_SESSION['uid']; ?>&price=<?php echo $r['price']; ?>">Add Cart</a></h5>
+
+									<?php
+									}
+									else
+									{
+									?>
+										<a href="login.php">Add Cart</a>
+									<?php } ?>
+
+										</div>
+									</div>
+								</div>
+								<?php } ?>
+							</div>	
+						</div>
+
+
+						<div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+							<div class="row">
+
+
+							<?php include "connect.php";
+								$s = mysqli_query($con,"select * from menu where category='kathiyawadi'");
+								while($r = mysqli_fetch_array($s))
+								{	
+								?>
+
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="admin/<?php echo $r['image']; ?>" class="img-fluid" alt="Image" style='width: 263px; height:170px;'>
+										<div class="why-text">
+											<h4><?php echo $r['title']; ?></h4>
+											<p><?php echo $r['description']; ?></p>
+											<h5>Rs <?php echo $r['price']; ?> /-</h5>
+											<h5 align="center">
+												<?php if(isset($_SESSION['uid']))
+									{
+									?>
+											<a href="addcart.php?pid=<?php echo $r['id']; ?>&uid=<?php echo $_SESSION['uid']; ?>&price=<?php echo $r['price']; ?>">Add Cart</a></h5>
+
+									<?php
+									}
+									else
+									{
+									?>
+										<a href="login.php">Add Cart</a>
+									<?php } ?>
+
+
+
+
+											</h5>
+										</div>
+									</div>
+								</div>
+								<?php } ?>
+								
+								
+							</div>
+							
+						</div>
+						<div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+							<div class="row">
+								
+								<?php include "connect.php";
+								$s = mysqli_query($con,"select * from menu where category='rajsthani'");
+								while($r = mysqli_fetch_array($s))
+								{	
+								?>
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="admin/<?php echo $r['image']; ?>" class="img-fluid" alt="Image" style='width: 263px; height:170px;'>
+										<div class="why-text">
+											<h4><?php echo $r['title']; ?></h4>
+											<p><?php echo $r['description']; ?></p>
+											<h5>Rs <?php echo $r['price']; ?> /-</h5>
+											<h5 align="center">
+												<?php if(isset($_SESSION['uid']))
+									{
+									?>
+											<a href="addcart.php?pid=<?php echo $r['id']; ?>&uid=<?php echo $_SESSION['uid']; ?>&price=<?php echo $r['price']; ?>">Add Cart</a></h5>
+
+									<?php
+									}
+									else
+									{
+									?>
+										<a href="login.php">Add Cart</a>
+									<?php } ?>
+
+
+											</h5>
+										</div>
+									</div>
+								</div>
+								<?php } ?>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>								
+
+	<!-- End Menu -->
+	
+	<?php include "footer.php" ; ?>
---
+++ admin/footer.php
@@ -0,0 +1,2 @@
+<br><br>
+<div class="footer"> Design & Developed By: Jp Sah,  Prashant Jha, Sachin Dhakal</div>
